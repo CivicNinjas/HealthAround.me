555b8e2a2275492aa5f142412cd27b03fd289b33
John Whitlock <John-Whitlock@ieee.org>
2014-02-21 16:10:44 -0600

    Add test JSON

--- healthdata/urls.py
+++ healthdata/urls.py
@@ -5,5 +5,6 @@ urlpatterns = patterns('healthdata.views',
     url(r'^$', TemplateView.as_view(template_name='healthdata/home.jinja2'),
         name='home'),
     url(r'^tryit/$', TemplateView.as_view(template_name='healthdata/try_it.jinja2'),
-        name='try_it')
+        name='try_it'),
+    url(r'^api/score/.*$', 'fake_api')
 )
--- healthdata/views.py
+++ healthdata/views.py
@@ -1,3 +1,13 @@
+import json
+import os.path
+
+from django.http import HttpResponse
+from django.conf import settings
 from django.shortcuts import render
 
-# Create your views here.
+def fake_api(request):
+    path = os.path.join(settings.BASE_DIR, 'TestData', 'SampleData.json')
+    content = json.load(file(path, 'r'))
+    response = HttpResponse(json.dumps(content, indent=2))
+    response['Content-Type'] = 'application/json'
+    return response
