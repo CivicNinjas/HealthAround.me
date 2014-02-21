4dba3d9a7b2934e9c3aecda2cb01a7b255523ef7
John Whitlock <John-Whitlock@ieee.org>
2014-02-21 15:51:00 -0600

    A silly demo page

--- healthdata/urls.py
+++ healthdata/urls.py
@@ -3,5 +3,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = patterns('healthdata.views',
     url(r'^$', TemplateView.as_view(template_name='healthdata/home.jinja2'),
-        name='home')
+        name='home'),
+    url(r'^tryit/$', TemplateView.as_view(template_name='healthdata/try_it.jinja2'),
+        name='try_it')
 )
