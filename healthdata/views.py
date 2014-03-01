3edce251cd954b6f508eabd3a56df55789daddc8
John Whitlock <John-Whitlock@ieee.org>
2014-03-01 16:33:22 -0600

    Enable jsonp w/ callback=name

--- healthdata/views.py
+++ healthdata/views.py
@@ -8,6 +8,12 @@ from django.shortcuts import render
 def fake_api(request):
     path = os.path.join(settings.BASE_DIR, 'TestData', 'SampleData.json')
     content = json.load(file(path, 'r'))
-    response = HttpResponse(json.dumps(content, indent=2))
-    response['Content-Type'] = 'application/json'
+    callback = request.GET.get('callback')
+    if callback:
+        out = '{}({});'.format(callback, json.dumps(content))
+        response = HttpResponse(out)
+        response['Content-Type'] = 'application/javascript'
+    else:
+        response = HttpResponse(json.dumps(content, indent=2))
+        response['Content-Type'] = 'application/json'
     return response
