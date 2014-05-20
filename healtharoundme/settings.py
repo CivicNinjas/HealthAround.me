b5197036d1a4b824c97aeccef4bf0bb0eda41769
harbieism <jow598@utulsa.edu>
2014-05-20 13:36:12 -0500

    Added data.

--- healtharoundme/settings.py
+++ healtharoundme/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = (
     'tastypie',
 
     # Our apps
+    'data',
     'healthdata',
 )
 
--- settings_override.example.py
+++ settings_override.example.py
@@ -12,9 +12,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'NAME': 'healtharoundme',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
+        'USER': 'healtharoundme',
+        'PASSWORD': 'healtharoundme',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
+POSTGIS_VERSION = ( 2, 1 )
