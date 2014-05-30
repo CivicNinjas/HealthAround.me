669c644790564741c3e13159d8a8c283b5231d4b
John Whitlock <John-Whitlock@ieee.org>
2014-05-30 09:54:05 -0500

    Rewrite std_dev_across_tracts
    - Now takes a list of target columns to be summed
    - Renamed, cleanup, PEP8

--- healthdata/utils.py
+++ healthdata/utils.py
@@ -1,19 +1,35 @@
 from boundaryservice.models import Boundary, BoundarySet
 from data.models import Census
 
-def stand_dev_form(total, target):
-	tract_set = BoundarySet.objects.all()[1]
-	ok_data = Boundary.objects.get(slug='oklahoma-state')
-	ok_data_new = Census.objects.filter(boundary=ok_data).values_list(total, target)
-	data = Census.objects.filter(boundary__set=tract_set).values_list(total, target)
-	ok_percentile = float(ok_data_new[0][1]/ok_data_new[0][0])
-	count = 0
-	total = 0.0
-	for tracts in data:
-		if tracts[0] != 0:
-			total += (float(tracts[1]/tracts[0]) - ok_percentile)**2
-			count += 1
-	total = (total/count)**(0.5)
-	return total
 
+def std_dev_across_tracts(total_col, target_cols):
+    '''
+    Calcuate the standard deviation across Oklahoma census tracts
 
+    total_col = Column with the population total
+    target_cols = Columns with the count meeting the criteria.
+    '''
+    # Get average for all Oklahoma
+    # TODO: Why did the slug change between database loads?
+    ok_state = Boundary.objects.get(set__slug='states', external_id=40)
+    ok_data = Census.objects.filter(
+        boundary=ok_state).values_list(total_col, *target_cols)[0]
+    ok_total = ok_data[0]
+    ok_values = ok_data[1:]
+    ok_percentile = sum(ok_values) / float(ok_total)
+
+    # Calculate standard deviation of tracts
+    tract_set = BoundarySet.objects.get(slug='census-tracts')
+    tract_rows = Census.objects.filter(
+        boundary__set=tract_set).values_list(total_col, *target_cols)
+    count = 0
+    total = 0.0
+    for row in tract_rows:
+        tract_total = row[0]
+        tract_values = row[1:]
+        if tract_total != 0:
+            tract_percentage = sum(tract_values) / float(tract_total)
+            total += (tract_percentage - ok_percentile)**2
+            count += 1
+    std_dev = (total/count)**(0.5)
+    return std_dev
