ec138a2487e7fe1dec2591b80fc0a66e9182a561
John Whitlock <John-Whitlock@ieee.org>
2014-06-10 22:01:19 -0500

    Add liberal CORS headers

--- healthdata/admin.py
+++ healthdata/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from mptt.admin import MPTTModelAdmin
 
-from .models import ProtoHealth, ScoreMetric, ScoreNode
+from .models import Feedback, ProtoHealth, ScoreMetric, ScoreNode
 
 
 class ScoreMetricAdmin(admin.ModelAdmin):
@@ -12,6 +12,7 @@ class ScoreNodeAdmin(MPTTModelAdmin):
     list_display = ('label', 'slug', 'rel_order', 'weight', 'metric')
     list_editable = ('slug', 'rel_order', 'weight')
 
+admin.site.register(Feedback)
 admin.site.register(ProtoHealth)
 admin.site.register(ScoreMetric, ScoreMetricAdmin)
 admin.site.register(ScoreNode, ScoreNodeAdmin)
