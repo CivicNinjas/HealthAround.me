0af61c428e21f2be6119908a956ac7759a25f6c0
harbieism <jow598@utulsa.edu>
2014-07-18 11:59:59 -0500

    Test setup retooled to make it more modular

--- healthdata/tests/test_utils.py
+++ healthdata/tests/test_utils.py
@@ -7,6 +7,7 @@ from django.contrib.gis.geos import GEOSGeometry
 from healthdata.utils import (
     fake_boundary,
     get_field_for_area,
+    highest_resolution_for_data
 )
 
 
@@ -43,7 +44,7 @@ class FakeBoundaryUtilTest(TestCase):
         self.assertEqual((-95.9905, 36.1495), boundary.centroid.coords)
 
 
-class ForAreaTest(TestCase):
+class ForAreaTestNoData(TestCase):
     def setUp(self):
         self.county_set = BoundarySet.objects.create(
             name='County',
@@ -88,6 +89,37 @@ class ForAreaTest(TestCase):
             kind='County',
             simple_shape=bound_two_shape,
             centroid="POINT (-95.75 36.25)")
+
+        self.state_set = BoundarySet.objects.create(
+            name='State',
+            slug='states',
+            kind_first=True,
+            last_updated='2014-05-21',
+            count=0,
+            metadata_fields=['GEOID'])
+
+        bound_three_shape = GEOSGeometry(
+            'MUlTIPOLYGON((('
+            '-97.0 35.5, -97.0 37.0, -94.0 37.0,'
+            '-94.0 35.5, -97.0 35.5)))'
+        )
+
+        self.bound_three = Boundary.objects.create(
+            slug='state-one',
+            name='State One',
+            set=self.state_set,
+            metadata={'GEOID': '40100'},
+            external_id='40100',
+            shape=bound_three_shape,
+            display_name='State One',
+            kind='State',
+            simple_shape=bound_three_shape,
+            centroid="POINT (-95.5 36.25)"
+        )
+
+
+class ForAreaCounty(object):
+    def setUp(self):
 
         Census.objects.create(
             boundary=self.bound_one,
@@ -328,112 +360,8 @@ class ForAreaTest(TestCase):
         )
 
 
-class GetFieldForAreaTest(ForAreaTest):
-
-    def test_get_field_for_area_intersection(self):
-        '''
-        Two areas, bound one (500 in field being tested) and bound two(1000 in
-        field being tested.) 1/25 of bound one is inside area_to_get_got, and
-        of bound two is inside area_to_get_got.  Therefore, the expcted result
-         is ((1/25) * 500) + ((9/25) * 1000) = 380
-        '''
-        area_to_get_got = GEOSGeometry(
-            'MUlTIPOLYGON(((-95.95 36.15, -95.45 36.15,'
-            '-95.45 36.35,-95.95 36.35, -95.95 36.15)))')
-        self.assertEqual(get_field_for_area(
-            area_to_get_got,
-            'B19058_002E',
-            Census
-        ), 380)
-
-    def test_get_field_for_area_inside(self):
-        '''
-        In this test, area_to_get_got is completely inside bound two and
-        is equal to 1/25 of bound two's area. Bound two has a population
-        of 1000 in the field we are getting. Therefore, the expected
-        result is equal to (1/25)* 1000 = 40
-        '''
-        area_to_get_got = GEOSGeometry(
-            'MUlTIPOLYGON(((-95.9 36.15, -95.8 36.15,'
-            '-95.8 36.25, -95.9 36.25, -95.9 36.15)))')
-        self.assertEqual(
-            get_field_for_area(
-                area_to_get_got,
-                'B19058_002E',
-                Census
-            ), 40)
-
-    def test_get_field_for_area_partially_outside_data(self):
-        '''
-        In this test, 2/3 of area_to_get_got is inside bound two, taking
-        up 2/25ths of bound two's total area, while the last 1/3 of
-        area_to_get_got is outside of it.  The algorithm will ignore this
-        overlap in to areas that don't contain any of the data we're looking
-        for, so we need only concern ourselves with the area inside bound two.
-        Expected Result: (2/25) * 1000 = 80
-        '''
-        area_to_get_got = GEOSGeometry(
-            'MUlTIPOLYGON(((-96.1 36.15, -95.8 36.15,'
-            '-95.8 36.25, -96.1 36.25, -96.1 36.15)))')
-        self.assertEqual(
-            get_field_for_area(
-                area_to_get_got,
-                'B19058_002E',
-                Census
-            ), 80)
-
-    def test_get_field_for_area_outside_data(self):
-        '''
-        In this test, area_to_get is defined as being somewhere in the arctic.
-        The get_field_for_area should return a string indicating that there is
-        no data in that area.
-        '''
-        area_to_get_got = GEOSGeometry(
-            'MUlTIPOLYGON(((-51.0 81.0, -52.0 81.0,'
-            '-52.0 82.0, -51.0 82.0, -51.0 81.0)))')
-        self.assertEqual(
-            get_field_for_area(
-                area_to_get_got,
-                'B19058_002E',
-                Census
-            ), "No data for that field in given area.")
-
-
-class HighestResDataWithout(ForAreaTest):
+class ForAreaState(object):
     def setUp(self):
-        super(HighestResDataWithout, self).setUp()
-
-        self.state_set = BoundarySet.objects.create(
-            name='State',
-            slug='states',
-            kind_first=True,
-            last_updated='2014-05-21',
-            count=0,
-            metadata_fields=['GEOID'])
-
-        bound_three_shape = GEOSGeometry(
-            'MUlTIPOLYGON((('
-            '-97.0 35.5, -97.0 37.0, -94.0 37.0,'
-            '-94.0 35.5, -97.0 35.5)))'
-        )
-
-        self.bound_three = Boundary.objects.create(
-            slug='state-one',
-            name='State One',
-            set=self.state_set,
-            metadata={'GEOID': '40100'},
-            external_id='40100',
-            shape=bound_three_shape,
-            display_name='County Two',
-            kind='County',
-            simple_shape=bound_three_shape,
-            centroid="POINT (-95.5 36.25)"
-        )
-
-
-class HighestResDataWith(HighestResDataWithout):
-    def setUp(self):
-        super(HighestResDataWith, self).setUp()
         Census.objects.create(
             boundary=self.bound_three,
             logical_num=4846,
@@ -551,4 +479,124 @@ class HighestResDataWith(HighestResDataWithout):
             B25091_020E=0.00,
             B25091_021E=0.00,
             B25091_022E=0.00,
+        )
+
+
+class GetFieldForAreaTest(ForAreaTestNoData, ForAreaCounty):
+
+    def setUp(self):
+        ForAreaTestNoData.setUp(self)
+        ForAreaCounty.setUp(self)
+
+    def test_get_field_for_area_intersection(self):
+        '''
+        Two areas, bound one (500 in field being tested) and bound two(1000 in
+        field being tested.) 1/25 of bound one is inside area_to_get_got, and
+        of bound two is inside area_to_get_got.  Therefore, the expcted result
+         is ((1/25) * 500) + ((9/25) * 1000) = 380
+        '''
+        area_to_get_got = GEOSGeometry(
+            'MUlTIPOLYGON(((-95.95 36.15, -95.45 36.15,'
+            '-95.45 36.35,-95.95 36.35, -95.95 36.15)))')
+        self.assertEqual(get_field_for_area(
+            area_to_get_got,
+            'B19058_002E',
+            Census
+        ), 380)
+
+    def test_get_field_for_area_inside(self):
+        '''
+        In this test, area_to_get_got is completely inside bound two and
+        is equal to 1/25 of bound two's area. Bound two has a population
+        of 1000 in the field we are getting. Therefore, the expected
+        result is equal to (1/25)* 1000 = 40
+        '''
+        area_to_get_got = GEOSGeometry(
+            'MUlTIPOLYGON(((-95.9 36.15, -95.8 36.15,'
+            '-95.8 36.25, -95.9 36.25, -95.9 36.15)))')
+        self.assertEqual(
+            get_field_for_area(
+                area_to_get_got,
+                'B19058_002E',
+                Census
+            ), 40)
+
+    def test_get_field_for_area_partially_outside_data(self):
+        '''
+        In this test, 2/3 of area_to_get_got is inside bound two, taking
+        up 2/25ths of bound two's total area, while the last 1/3 of
+        area_to_get_got is outside of it.  The algorithm will ignore this
+        overlap in to areas that don't contain any of the data we're looking
+        for, so we need only concern ourselves with the area inside bound two.
+        Expected Result: (2/25) * 1000 = 80
+        '''
+        area_to_get_got = GEOSGeometry(
+            'MUlTIPOLYGON(((-96.1 36.15, -95.8 36.15,'
+            '-95.8 36.25, -96.1 36.25, -96.1 36.15)))')
+        self.assertEqual(
+            get_field_for_area(
+                area_to_get_got,
+                'B19058_002E',
+                Census
+            ), 80)
+
+    def test_get_field_for_area_outside_data(self):
+        '''
+        In this test, area_to_get is defined as being somewhere in the arctic.
+        The get_field_for_area should return a string indicating that there is
+        no data in that area.
+        '''
+        area_to_get_got = GEOSGeometry(
+            'MUlTIPOLYGON(((-51.0 81.0, -52.0 81.0,'
+            '-52.0 82.0, -51.0 82.0, -51.0 81.0)))'
+        )
+        self.assertEqual(
+            get_field_for_area(
+                area_to_get_got,
+                'B19058_002E',
+                Census
+            ), "No data for that field in given area."
+        )
+
+
+class HighestResStateNoTests(ForAreaTestNoData, ForAreaCounty):
+    def setUp(self):
+        ForAreaTestNoData.setUp(self)
+        ForAreaCounty.setUp(self)
+
+    def test_state_and_county_no_state_data(self):
+        '''
+        In this test, area_to_get_got is defined as being located inside both
+        a county and a state, where a census table containing the data we are
+        looking for is paired with the county but not the state. Should
+        return county.
+        '''
+        area_to_get_got = GEOSGeometry(
+            'MUlTIPOLYGON(((-95.32 36.4, -95.31 36.4,'
+            '-95.31 36.45, -95.32 36.45, -95.32 36.4)))'
+        )
+        self.assertEqual(
+            highest_resolution_for_data(
+                area_to_get_got,
+                'B19058_002E',
+                Census
+            ), "County"
+        )
+
+    def test_state_no_state_data(self):
+        '''
+        In this test, area_to_get_got is defined as being located inside a
+        state and no other boundaries, where the data we are looking for
+        is not paired with the state's boundary.  Should return None.
+        '''
+        area_to_get_got = GEOSGeometry(
+            'MUlTIPOLYGON(((-95.45 36.8, -95.5 36.8,'
+            '-95.5 36.9, -95.45 36.9, -95.45 36.8)))'
+        )
+        self.assertEqual(
+            highest_resolution_for_data(
+                area_to_get_got,
+                'B19058_002E',
+                Census
+            ), None
         )
